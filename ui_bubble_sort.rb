7d13bcda667d4d6abeecb1b1bcba32bb5d01ca3d
Katarzyna Mildner <itsyehudit@gmail.com>
2019-09-10 01:35:54 +0200

    Wrapped bubble_sort UI into a class

--- ui_bubble_sort.rb
+++ ui_bubble_sort.rb
@@ -1,15 +1,25 @@
 require_relative 'bubble_sort'
 
-arr_in = []
-
-loop do
-  puts "Pick a number - type \"stop\" if you are done:"
-  input = gets.chomp
-  unless input == "stop"
-    input = input.to_i
-    arr_in.push(input)
+class UIBubbleSort
+  def initialize
+    @sort = BubbleSort.new
   end
-  break if input == "stop"
+
+  def sort_input
+    arr_in = []
+    loop do
+      puts "Pick a number - type \"stop\" if you are done:"
+      input = gets.chomp
+      unless input == "stop"
+        input = input.to_i
+        arr_in.push(input)
+      end
+      break if input == "stop"
+    end
+
+    puts "Your array #{arr_in} is sorted: #{@sort.bubble_sort(arr_in)}."
+  end
 end
 
-puts "Your array #{arr_in} is sorted: #{bubble_sort(arr_in)}."
+bubble_sort = UIBubbleSort.new
+bubble_sort.sort_input
