85b9b92f9f0d8ab34175f97f2cc17494743b8086
Katarzyna Mildner <itsyehudit@gmail.com>
2019-09-09 09:12:15 +0200

    Add UI

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -13,3 +13,17 @@ def bubble_sort(arr)
   end
   return arr
 end
+
+arr_in = []
+
+loop do
+  puts "Pick a number - type \"stop\" if you are done:"
+  input = gets.chomp
+  unless input == "stop"
+    input = input.to_i
+    arr_in.push(input)
+  end
+  break if input == "stop"
+end
+
+puts "Your array #{arr_in} is sorted: #{bubble_sort(arr_in)}."
