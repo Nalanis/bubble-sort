273db8276d09fd79071ffa898e6b7a2d85c7144e
Katarzyna Mildner <itsyehudit@gmail.com>
2019-09-10 12:06:05 +0200

    Test: sorting input by bubble_sort.rb

--- spec/bubble_sort_spec.rb
+++ spec/bubble_sort_spec.rb
@@ -1,5 +1,10 @@
 require_relative '../bubble_sort'
 
 RSpec.describe BubbleSort do
-  it "sorts input array in ascending order"
+  it "sorts input array in ascending order" do
+    example = BubbleSort.new
+    bubble_sort_input = [1, 17, 11, 9, 23, 512, 5, 2]
+
+    expect(example.bubble_sort(bubble_sort_input)).to eq([1, 2, 5, 9, 11, 17, 23, 512])
+  end
 end
